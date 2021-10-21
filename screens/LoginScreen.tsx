ae22dd2e3427922a897d1f149e82f1547674defc
Viktor Nyström <95viknys@skola.boras.se>
2021-10-21 13:11:59 +0200

    Trying out navigation to household screen

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,33 +1,35 @@
 import { useTheme } from "@react-navigation/native";
 import React, { useState } from "react";
-import { Button, StyleSheet, Text, View, Image, TextInput } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { CustomNavigateButton } from "../component/CustomNavigateButton";
+import { mockedUserData } from '../data/data';
 import { RootStackScreenProps } from "../navigation/RootStackNavigator";
 
 export default function LoginScreen({ navigation }: RootStackScreenProps<"Login">) {
     const { colors } = useTheme();
     const [userText, onUserTextChange] = useState('');
     const [userPassword, onUserPasswordChange] = useState('');
+    const user = mockedUserData[0];
 
     return (
         <SafeAreaView style={[{ backgroundColor: colors.background }]}>
             <Text style={styles.loginText}>Logga in</Text>
             <TextInput
-                onChangeText={onUserTextChange}
                 style={styles.input}
                 value={userText}
+                onChangeText={onUserTextChange}
                 placeholder='Epost / användarnamn'
             />
             <TextInput
                 style={styles.input}
                 value={userPassword}
                 onChangeText={onUserPasswordChange}
-                placeholder='Lösenord'
                 secureTextEntry={true}
+                placeholder='Lösenord'
             />
             <View style={styles.viewStyle}>
-                <CustomNavigateButton buttonText="Logga in" goto={() => { }} />
+                <CustomNavigateButton buttonText="Logga in" goto={() => navigation.navigate('Household', {user})} />
             </View>
         </SafeAreaView>
     );
@@ -35,7 +37,6 @@ export default function LoginScreen({ navigation }: RootStackScreenProps<"Login"
 
 const styles = StyleSheet.create({
     loginText: {
-        marginTop: 100,
         fontSize: 40,
         marginLeft: 10,
         marginBottom: 10
