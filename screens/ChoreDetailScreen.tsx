6a423ae821fc1f4f1e51d6916a6c31ef003fbdc9
Viktor Nyström <95viknys@skola.boras.se>
2021-10-28 14:57:24 +0200

    Moved styling css to own file for chore details

--- screens/ChoreDetailScreen.tsx
+++ screens/ChoreDetailScreen.tsx
@@ -1,166 +1,66 @@
-import { Text, View, StyleSheet } from 'react-native';
-import React, { useEffect } from 'react';
-import { RootStackScreenProps } from '../navigation/RootStackNavigator';
-import { useAppSelector } from '../store/store';
-import { getChoreByIdSelector } from '../store/household/householdSelectors';
 import { useTheme } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { Text, View } from 'react-native';
 import { CustomNavigateButton } from '../component/CustomNavigateButton';
+import { RootStackScreenProps } from '../navigation/RootStackNavigator';
+import { getChoreByIdSelector } from '../store/household/householdSelectors';
+import { useAppSelector } from '../store/store';
+import { choreStyles } from '../style/choreDetailStyle';
 
 export default function ChoreDetailScreen({ navigation, route }: RootStackScreenProps<'ChoreDetail'>) {
     const { colors } = useTheme();
     const chore = useAppSelector((state) => getChoreByIdSelector(state, route.params.choreId, route.params.householdId));
 
-    // Update page title to chore name
     useEffect(() => {
         navigation.setOptions({ title: chore?.name });
     }, []);
 
     return (
-        <View style={[{ backgroundColor: colors.border }, styles.root]}>
+        <View style={[{ backgroundColor: colors.background }, choreStyles.root]}>
             <Text
                 style={[
                     {
                         color: colors.text,
-                        backgroundColor: colors.background
+                        backgroundColor: colors.card
                     },
-                    styles.descriptionContainer,
-                    styles.bottomMargin
+                    choreStyles.descriptionContainer,
+                    choreStyles.bottomMargin
                 ]}
             >
                 {chore?.description}
             </Text>
 
-            <View style={[{ backgroundColor: colors.background }, styles.frequencyContainer]}>
-                <View style={styles.testContainer}>
+            <View style={[{ backgroundColor: colors.card }, choreStyles.frequencyContainer]}>
+                <View style={choreStyles.middleContainer}>
                     <View>
-                        <Text style={[{ color: colors.text }, styles.testText]}>Återkommer: </Text>
+                        <Text style={[{ color: colors.text }, choreStyles.recurrenceText]}>Återkommer: </Text>
                     </View>
-                    <View>
-                        <Text style={[{ color: colors.text }]}> var </Text>
-                    </View>
-                    <View style={styles.freqNrContainer}>
-                        <Text style={styles.frequencyNumberText}>{chore?.frequency}</Text>
-                    </View>
-                    <View>
-                        <Text style={[{ color: colors.text }, styles.dayPadding]}> dag </Text>
+                    <View style={choreStyles.innerContainer}>
+                        <View>
+                            <Text style={[{ color: colors.text }]}> var </Text>
+                        </View>
+                        <View style={choreStyles.freqNrContainer}>
+                            <Text style={choreStyles.frequencyNumberText}>{chore?.frequency}</Text>
+                        </View>
+                        <View>
+                            <Text style={[{ color: colors.text }]}> dag </Text>
+                        </View>
                     </View>
                 </View>
             </View>
 
-            <View style={[{ backgroundColor: colors.background }, styles.energyContainer]}>
-                <View style={styles.columnStyle}>
-                    <Text style={[{ color: colors.text }, styles.text]}>Värde: </Text>
-                    <Text style={[styles.valueDescription]}>Hur energikrävande är sysslan?</Text>
+            <View style={[{ backgroundColor: colors.card }, choreStyles.energyContainer]}>
+                <View style={choreStyles.columnStyle}>
+                    <Text style={[{ color: colors.text }, choreStyles.text]}>Värde: </Text>
+                    <Text style={[choreStyles.valueDescription]}>Hur energikrävande är sysslan?</Text>
                 </View>
-                <View style={styles.energyNrContainer}>
-                    <Text style={[{ color: colors.text }, styles.energyText]}>{chore?.frequency}</Text>
+                <View style={choreStyles.energyNrContainer}>
+                    <Text style={[{ color: colors.text }, choreStyles.energyText]}>{chore?.frequency}</Text>
                 </View>
+            </View>
+            <View style={choreStyles.buttonContainer}>
+                <CustomNavigateButton goto={() => navigation.goBack()} buttonText='Klar' />
             </View>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    root: {
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    testContainer: {
-        width: '95%',
-        borderRadius: 10,
-        margin: 10,
-        justifyContent: 'center',
-        flexDirection: 'row',
-        elevation: 20,
-        alignContent: 'center',
-        alignItems: 'center'
-    },
-    testText: {
-        margin: 5,
-        fontSize: 20,
-        fontWeight: 'bold',
-        paddingRight: '41%'
-    },
-    descriptionContainer: {
-        width: '95%',
-        height: '20%',
-        borderRadius: 10,
-        padding: 5,
-        elevation: 20,
-        fontSize: 20
-    },
-    frequencyContainer: {
-        width: '95%',
-        borderRadius: 10,
-        justifyContent: 'space-between',
-        alignItems: 'baseline',
-        flexDirection: 'row',
-        elevation: 20,
-        marginBottom: 15
-    },
-    energyContainer: {
-        width: '95%',
-        borderRadius: 10,
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-        elevation: 20,
-        marginBottom: 15
-    },
-    text: {
-        margin: 5,
-        fontSize: 20,
-        fontWeight: 'bold',
-        paddingTop: 4,
-        paddingRight: 4,
-        paddingLeft: 4
-    },
-    frequencyText: {
-        margin: 5,
-        fontSize: 20,
-        padding: 4
-    },
-    frequencyNumberText: {
-        fontSize: 20
-    },
-    freqNrContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 4,
-        borderRadius: 100,
-        backgroundColor: 'red',
-        width: 30,
-        height: 30
-    },
-    energyText: {
-        fontSize: 15
-    },
-    energyNrContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 5,
-        marginRight: 15,
-        alignSelf: 'center',
-        padding: 4,
-        borderRadius: 100,
-        backgroundColor: 'red',
-        width: 30,
-        height: 30
-    },
-    bottomMargin: {
-        marginBottom: 60
-    },
-    valueDescription: {
-        fontSize: 12,
-        color: 'grey',
-        marginTop: -10,
-        margin: 10
-    },
-    columnStyle: {
-        justifyContent: 'center',
-        flexDirection: 'column'
-    },
-    dayPadding: {
-        paddingRight: 10
-    }
-});
