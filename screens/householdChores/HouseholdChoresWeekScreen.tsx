1f840894f2f0ca63cc130fb0817beacbdc924c38
Grodan Bool <71007996+GrodanBool@users.noreply.github.com>
2021-11-03 15:03:32 +0100

    Update HouseholdChoresWeekScreen.tsx

--- screens/householdChores/HouseholdChoresWeekScreen.tsx
+++ screens/householdChores/HouseholdChoresWeekScreen.tsx
@@ -29,7 +29,7 @@ export default function HouseholdChoresWeekScreen(props: Props) {
             <View style={styles.root}>
                 {/* TODO: route and navigation may be pased as props to RenderChores -> ChoreButton */}
                 {/* TODO: For more view in choreSlider, only rename label for those screens */}
-                <RenderChores prop={props} label={'Week'} currentHousehold={currentHousehold} members={memberList} editChore={toggleEdit} />
+                <RenderChores navigation={props} label={'Week'} currentHousehold={currentHousehold} members={memberList} editChore={toggleEdit} />
                 {!toggleEdit && currentMember?.memberType === 'owner' && (
                     <View style={[styles.buttons, { justifyContent: 'space-between' }]}>
                         <CustomPlusButton goto={() => console.log('Lägg till en syssla')} buttonText={'Lägg till'} />
